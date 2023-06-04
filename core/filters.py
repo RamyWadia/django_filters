1bd7421dbf786b1aea6441d9b53d7e508b74bbac
Ramy Atalla <ramywadia@gmail.com>
2023-06-04 21:56:06 +0300

    override the genre filter to be using multiple choice field, and check box select multiple widget

--- core/filters.py
+++ core/filters.py
@@ -1,14 +1,20 @@
 import django_filters
 from books.models import Book
+from django import forms
 
 
 class BookFilter(django_filters.FilterSet):
+    # override the price field to make it rangeFilter
     price = django_filters.RangeFilter()
+    # override the genre field to use multiple choice values
+    genre = django_filters.MultipleChoiceFilter(
+        choices=Book.GenreChoices.choices,
+        widget=forms.CheckboxSelectMultiple,
+    )
 
     class Meta:
         model = Book
         fields = {
             "name": ["icontains"],
             "author__name": ["istartswith"],
-            "genre": ["exact"],
         }
--- core/serializers.py
+++ core/serializers.py
@@ -4,7 +4,7 @@ from books.models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     # create an author_name to get the author.name as it is a
-    # forgein key field
+    # foreign key field
     author_name = serializers.CharField(source="author.name")
     # specifying the genre to get the value not the "one letter"
     genre = serializers.CharField(source="get_genre_display")
